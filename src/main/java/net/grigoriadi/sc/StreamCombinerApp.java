21f01fff52f6a3fd195653bd40bf2bca6be3501e
Roman Grigoriadi <bravehorsie@gmail.com>
2015-08-02 17:13:21 +0200

    Added maven exec plugin, reformatted text in main app.

--- src/main/java/net/grigoriadi/sc/StreamCombinerApp.java
+++ src/main/java/net/grigoriadi/sc/StreamCombinerApp.java
@@ -25,28 +25,33 @@ public class StreamCombinerApp {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Usage:");
-            System.out.println("Run a program with arguments in format \"host:port\" separated by spaces.");
-            System.out.println("As many clients will be spawned, as many words separated by spaces are provided" +
-                    " for a single localhost:constant_port.");
-            System.out.println("(Since app is actually runs one generating server on a localhost single port, " +
-                    "you can type any words separated by spaces).");
+            System.out.println("|------------------------------------------------------------------------------|");
+            System.out.println("| Usage:                                                                       |");
+            System.out.println("| Run a program with arguments in format \"host:port\" separated by spaces.    |");
+            System.out.println("| As many clients will be spawned, as many words separated by spaces           |");
+            System.out.println("| are provided for a single localhost:constant_port.                           |");
+            System.out.println("| (Since app is actually runs one generating server                            |");
+            System.out.println("| on a localhost single port, you can type any words separated by spaces).     |");
+            System.out.println("|------------------------------------------------------------------------------|");
             System.exit(0);
         }
         StreamCombinerApp app = new StreamCombinerApp();
-        System.out.println("Pressing enter will start a server, clients and a single queue worker, generating JSON.");
-        System.out.println("Pressing enter again will terminate a server, all clients and stop a queue worker after queue is drained.");
-        System.out.println("Sum of amounts received, should always equal sum of amounts generated, " +
-                "besides the fact if application is terminated before completion.");
-        System.out.println("Generating data amount per client connection could be specified by a constant in AppContext." +
-                " (Defaulted to 100.000 per client)");
+        System.out.println("|------------------------------------------------------------------------------|");
+        System.out.println("| Pressing enter will start a server, clients and a single queue worker.       |");
+        System.out.println("| Pressing enter again will terminate all spawned threads                      |");
+        System.out.println("| and stop a queue worker after queue of remaining items is drained.           |");
+        System.out.println("| Sum of amounts received, should always equal sum of amounts generated,       |");
+        System.out.println("| besides the fact if application is terminated before completion.             |");
+        System.out.println("| Generating data amount per client connection could be specified              |");
+        System.out.println("| in app.properties files. (Defaulted to 100.000 x 10 clients)                 |");
+        System.out.println("|------------------------------------------------------------------------------|");
         app.waitForKeyPress(() -> LOG.debug("Starting server and clients..."));
         app.runServer();
-        //in the description of the app it is actually mentioned to run clients based on N hosts:ports
-        //since dummy server generating data is listen on one single localhost port it doesn't make sense
-        //can be effortlessly changed to
+        //in the description of the app it is actually mentioned to run clients based on N hosts:ports.
+        //since dummy server generating data listens on one single localhost port it doesn't make sense to do so.
         app.runClients(args.length);
         app.runQueueWorker();
+        //terminate app on second enter press.
         app.waitForKeyPress(app::terminate);
 
         LOG.debug("Exiting main thread");
@@ -65,6 +70,7 @@ public class StreamCombinerApp {
     private void runQueueWorker() {
         QueueWorker task = new QueueWorker();
         Future<?> workerFuture = executorService.submit(task);
+        //if worker is ahead of all clients and is blocking on queue.take() notify it to interrupt.
         AppContext.getInstance().getClientRegistry().setAllClientsShutDownListener(()-> workerFuture.cancel(true));
     }
 
